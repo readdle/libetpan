b940d9a64a422edcf94c724f5c44c6a46a5f2a8a
Andrew Druk <adruk@readdle.com>
2021-07-29 11:36:02 +0300

    Dev: fix openssl paths in swift android toolchain 5.4

--- build-android/dependencies/openssl/build.sh
+++ build-android/dependencies/openssl/build.sh
@@ -10,16 +10,16 @@ mkdir -p $package_name-$build_version/libs/armeabi-v7a
 mkdir -p $package_name-$build_version/libs/x86_64
 mkdir -p $package_name-$build_version/libs/x86
 
-cp -r $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/openssl ./$package_name-$build_version/include/
+cp -r $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-aarch64/openssl ./$package_name-$build_version/include/
 
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/armv7/libcrypto.a ./$package_name-$build_version/libs/armeabi-v7a
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/armv7/libssl.a ./$package_name-$build_version/libs/armeabi-v7a
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/aarch64/libcrypto.a ./$package_name-$build_version/libs/arm64-v8a
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/aarch64/libssl.a ./$package_name-$build_version/libs/arm64-v8a
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/i686/libcrypto.a ./$package_name-$build_version/libs/x86
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/i686/libssl.a ./$package_name-$build_version/libs/x86
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/x86_64/libcrypto.a ./$package_name-$build_version/libs/x86_64
-cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/x86_64/libssl.a ./$package_name-$build_version/libs/x86_64
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-armv7/android/libcrypto.a ./$package_name-$build_version/libs/armeabi-v7a
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-armv7/android/libssl.a ./$package_name-$build_version/libs/armeabi-v7a
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-aarch64/android/libcrypto.a ./$package_name-$build_version/libs/arm64-v8a
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-aarch64/android/libssl.a ./$package_name-$build_version/libs/arm64-v8a
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-i686/android/libcrypto.a ./$package_name-$build_version/libs/x86
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-i686/android/libssl.a ./$package_name-$build_version/libs/x86
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-x86_64/android/libcrypto.a ./$package_name-$build_version/libs/x86_64
+cp $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-x86_64/android/libssl.a ./$package_name-$build_version/libs/x86_64
 
 cd "$current_dir"
 zip -qry "$package_name-$build_version.zip" "$package_name-$build_version"
